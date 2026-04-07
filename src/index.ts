3c207a61cc23b95e2f95713574b6ae1cc12caea0
Sebastian Guerrero <wesdell17@gmail.com>
2026-04-07 10:43:12 -0500

    Ignore builded package and directly print the URL to reach the server

--- src/index.ts
+++ src/index.ts
@@ -37,6 +37,6 @@ const port = process.env.PORT || 3000;
 
 if (!isProduction) {
   app.listen(port, () => {
-    console.log(`Server running at port: ${port}`)
+    console.log(`Server running at: http://localhost:${port}`)
   })
 }
