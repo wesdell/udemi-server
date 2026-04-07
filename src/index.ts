751e32467fb3fd5cbeb906cd4fd5b16dbd81bd8d
Sebastian Guerrero <wesdell17@gmail.com>
2026-04-07 10:32:26 -0500

    Setup server to check if it responds

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import * as dynamoose from "dynamoose";
+
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === "production";
+if (!isProduction) {
+  dynamoose.aws.ddb.local();
+}
+
+const app = express();
+
+app.use(express.json());
+
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+app.use(morgan("common"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(cors());
+
+// Routes
+app.get("/", (_, res) => {
+  res.send("Hello, World!")
+});
+
+// Server
+const port = process.env.PORT || 3000;
+
+if (!isProduction) {
+  app.listen(port, () => {
+    console.log(`Server running at port: ${port}`)
+  })
+}
