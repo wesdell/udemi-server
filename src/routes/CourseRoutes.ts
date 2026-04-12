0092b4e156d59b50e103cf2f05a31cf27682da2b
Sebastian Guerrero <wesdell17@gmail.com>
2026-04-12 17:08:47 -0500

    Add course routes and prepare update endpoint when receiving media file changes

--- src/routes/CourseRoutes.ts
+++ src/routes/CourseRoutes.ts
@@ -1,11 +1,19 @@
 import express from 'express';
+import multer from "multer";
 
-import { getCourse, getCourses } from '../controllers/CourseController';
+import { createCourse, deleteCourse, getCourse, getCourses, updateCourse } from '../controllers/CourseController';
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/", getCourses);
 router.get("/:courseId", getCourse);
+
+router.post("/", createCourse);
+
+router.put("/:courseId", upload.single("image"), updateCourse);
+
+router.delete("/:courseId", deleteCourse)
 
 
 export default router;
